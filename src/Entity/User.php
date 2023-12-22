e028923054ca180188d85d20285b6d65ad708b66
BartoszLauks <bartosz.lauksstu@interia.pl>
2023-12-22 17:50:09 +0100

    Added restart password logic with secure standards and remember me features

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,6 +15,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[ORM\HasLifecycleCallbacks()]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
