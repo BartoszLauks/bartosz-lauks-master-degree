386bb4c8767531cd38c98e6056a02fe75e9dd33e
BartoszLauks <bartosz.lauksstu@interia.pl>
2023-12-22 16:29:18 +0100

    Added css and information to footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/dedication', name: 'app_dedication')]
+    public function dedication(): Response
+    {
+        return $this->render('home/dedication.html.twig');
     }
 }
